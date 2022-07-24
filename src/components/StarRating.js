544c791e72b47486840be1ad8d5dd38748ca6f84
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-07-24 22:00:34 +0300

    add useState hook

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Star from "./Star";
 
 const createArray = (lenght) => [...Array(lenght)];
 
-const StarRating = ({ tatalStars = 5 }) => {
-  return createArray(tatalStars).map((n, i) => (
-    <Star selected={true} key={i} />
-  ));
+const StarRating = ({ totalStars = 5 }) => {
+  const [selectedStars] = useState(4);
+  return (
+    <>
+      {createArray(totalStars).map((n, i) => (
+        <Star key={i} selected={selectedStars > i} />
+      ))}
+      <p>
+        {selectedStars} of {totalStars} stars
+      </p>
+    </>
+  );
 };
 
 export default StarRating;
