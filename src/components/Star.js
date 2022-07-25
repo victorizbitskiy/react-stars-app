f01590021a6bb9b6d744528b941e7f380931dcde
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-07-25 22:28:42 +0300

    add onSelect

--- src/components/Star.js
+++ src/components/Star.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { FaStar } from 'react-icons/fa';
 
-const Star = ({ selected = false }) => {
+const Star = ({ selected = false, onSelect }) => {
   return (
-    <FaStar color={selected ? 'gold' : 'gray'} />)
+    <FaStar color={selected ? 'gold' : 'gray'} onClick={onSelect} />)
 };
 
 export default Star;
--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -4,11 +4,15 @@ import Star from "./Star";
 const createArray = (lenght) => [...Array(lenght)];
 
 const StarRating = ({ totalStars = 5 }) => {
-  const [selectedStars] = useState(4);
+  const [selectedStars, setSelectedStars] = useState(0);
   return (
     <>
       {createArray(totalStars).map((n, i) => (
-        <Star key={i} selected={selectedStars > i} />
+        <Star
+          key={i}
+          selected={selectedStars > i}
+          onSelect={() => setSelectedStars(i + 1)}
+        />
       ))}
       <p>
         {selectedStars} of {totalStars} stars
