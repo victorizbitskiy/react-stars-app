9d0f3c1d64d7e03bc1f34ac21bf7d8821e5d60ea
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-09-09 14:04:34 +0300

    fix bug

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ColorProvider from '../hooks/color-hooks';
 import AddColorForm from './AddColorForm'
 import ColorList from './ColorList';
+import { Routes, Route } from "react-router-dom"
 
 const App = () => {
   return (
     <ColorProvider >
       <AddColorForm />
-      <ColorList />
+      <Routes>
+        <Route path="/" element={<ColorList />} />
+      </Routes>
+
     </ColorProvider>
   );
 };
