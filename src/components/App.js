bd443ecc2b3bc93bccc5cf0abea4fe5429d2c347
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-09-09 13:32:44 +0300

    end

--- src/components/App.js
+++ src/components/App.js
@@ -2,20 +2,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import ColorProvider from '../hooks/color-hooks';
 import AddColorForm from './AddColorForm'
-import ColorDetails from './ColorDetails';
 import ColorList from './ColorList';
 
 const App = () => {
-
   return (
     <ColorProvider>
       <AddColorForm />
       <Routes>
-        <Route path="/" element={<h2>Start page</h2>} />
-        <Route path="/colorlist" element={<ColorList />} >
-          <Route path=":id" element={<ColorDetails />} />
-        </Route>
-        <Route path="*" element={<h2>Ресурс не найден</h2>} />
+        <Route path="/" element={<ColorList />} />
       </Routes>
     </ColorProvider>
   );
