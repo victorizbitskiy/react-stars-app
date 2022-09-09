d8558ad705596d899c93ddb48f224e6fb77c7b72
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-09-09 13:57:58 +0300

    fix bug

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import ColorProvider from '../hooks/color-hooks';
 import AddColorForm from './AddColorForm'
 import ColorList from './ColorList';
 
 const App = () => {
   return (
-    <>
+    <ColorProvider >
       <AddColorForm />
       <ColorList />
-    </>
+    </ColorProvider>
   );
 };
 
